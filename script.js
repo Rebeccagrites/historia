05c5f51e1a093c790ef2d7aa08fed408671a2904
Rebeccagrites <rebecca.grites@escola.pr.gov.br>
2025-12-02 20:25:35 -0300

    hhh

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+// Música de fundo
+const music = document.getElementById("bgMusic");
+const musicBtn = document.getElementById("musicBtn");
+
+musicBtn.addEventListener("click", () => {
+    if (music.paused) {
+        music.play();
+        musicBtn.textContent = "⏸ Pausar Música";
+    } else {
+        music.pause();
+        musicBtn.textContent = "▶️ Tocar Música";
+    }
+});
+
+
+// Carrossel
+let currentSlide = 0;
+const slides = document.querySelectorAll(".carousel-images img");
+const totalSlides = slides.length;
+
+function updateCarousel() {
+    const carouselImages = document.querySelector(".carousel-images");
+    carouselImages.style.transform = `translateX(${-currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    updateCarousel();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+}
+
+// Auto-play do carrossel
+setInterval(nextSlide, 5000);
 
